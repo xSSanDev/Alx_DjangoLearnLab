3a849058e85059f9ca0825fcab36b41018304092
xSSanDev <xssanass@gmail.com>
2024-12-05 16:40:10 +0100

    .

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+# blog/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
