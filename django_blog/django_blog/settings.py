96a2ef46ca54cd6ab2c23df57f30f59742aeae7d
xSSanDev <xssanass@gmail.com>
2024-12-05 21:23:56 +0100

    .

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'your_db_name',
-        'USER': 'your_db_user',
-        'PASSWORD': 'your_db_password',
-        'HOST': 'your_db_host',
-        'PORT': 'your_db_port',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
